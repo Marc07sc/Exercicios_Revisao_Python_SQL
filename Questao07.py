5dfa40ff2b99b1cea16f3658356cd787833947a6
Marcello Silva <marcello.07sc@gmail.com>
2025-05-05 20:04:53 -0300

    Atualizando Código da Questão 7 (e incluindo alternativa)

--- Questao07.py
+++ Questao07.py
@@ -1,7 +1,11 @@
 # 7. Declare uma variável booleana que verifica se um número (entrada como
 # inteiro) é par, depois imprima o número e o resultado booleano.
 
-num = float(input("Digite um número:"))
+num = int(input("Digite um número inteiro:"))
+
+# Também é possível usar:
+# eh_par = True if num%2==0 else False
+# print(f"Você digitou {num}, que é um numéro {'par' if eh_par else 'ímpar'}")
 
 if num % 2 == 0:
     par = True
