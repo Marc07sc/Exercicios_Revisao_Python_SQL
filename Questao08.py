50967e13a74d8594763b0fec3c57e20337f1a8a7
Marcello Silva <marcello.07sc@gmail.com>
2025-05-05 20:18:30 -0300

    Atualizando questão 08 (e apresentando alternativas)

--- Questao08.py
+++ Questao08.py
@@ -4,10 +4,18 @@
 
 frase = input("Digite algo. Pode ser qualquer coisa.")
 
-if len(frase) > 5:
-    maior = True
-else:
-    maior = False
+# Alternativa 1
+# tem_mais_de_5 = len(frase)>5
+# print("É maior que 5? ", tem_mais_de_5)
 
-print(f"Você digitou '{frase}'.")
-print("Tamanho é maior que 5?", maior)
+# Alternativa 2
+# if len(frase) > 5:
+#     maior = True
+# else:
+#     maior = False
+# print(f"Você digitou '{frase}'.")
+# print("Tamanho é maior que 5?", maior)
+
+# Alternativa 3
+print(f"Você digitou '{frase}'.\n"
+      f"Tamanho é maior que 5? {'Sim' if len(frase) > 5 else 'Não'}.")
