8612ad96da829fc63c9e031e8bf9c94d0cd260b3
Marcello Silva <marcello.07sc@gmail.com>
2025-05-05 19:57:52 -0300

    Atualizando Questão 06

--- Questao06.py
+++ Questao06.py
@@ -1,8 +1,8 @@
 # 6. Crie um programa que converta uma temperatura de Celsius (entrada como
 # float) para Fahrenheit usando a fórmula F = C * 9/5 + 32 e imprima o resultado.
 
-Celsius = float(input("Digite uma temperatura em graus celsius:"))
+Celsius = float(input("Digite uma temperatura em graus celsius (ºC):"))
 
 Farenheit = Celsius * 9/5 + 32
 
-print(f"Esta temperatura em Farenheit é {Farenheit}.")
+print(f"Esta temperatura em Farenheit é {Farenheit:.1f} ºF.")
