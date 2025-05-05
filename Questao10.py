3ff948f741c4bd4901020f4c56553b43e3af2771
Marcello Silva <marcello.07sc@gmail.com>
2025-05-05 20:54:08 -0300

    Atualizando 10 e propondo alternativa

--- Questao10.py
+++ Questao10.py
@@ -1,5 +1,22 @@
-dicionario = {"Juliano": 7.0, "Roberto": 6.5, "Maria": 9.5}
+# 10.Faça um dicionário com 3 nomes de alunos como chaves e suas notas como
+# valores. Imprima cada aluno e sua nota em uma frase
 
+# Alternativa 1 - Especificando a quantidade de alunos e pedindo para inserir cada nome e nota
+
+# num_items = 3
+# alunos = {}
+#
+# for i in range(1, num_items+1):
+#     nome = input(f"Digite o nome do {i}º aluno.")
+#     nota = float(input(f"Digite a nota do {i}º aluno."))
+#     alunos[nome] = nota
+#
+# for nome, nota in alunos.items():
+#     print(f"O aluno de nome {nome} tirou nota {nota}.")
+
+# Alternativa 2 - Já inserindo cada aluno e nota
+
+dicionario = {"Juliano": 7.0, "Roberto": 6.5, "Maria": 9.5}
 print(dicionario)
 
 for nome, nota in dicionario.items():
