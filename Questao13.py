0c1e4f66ab4bb8a92e6eec3cf6cfdf37a9ee30f0
Marcello Silva <marcello.07sc@gmail.com>
2025-05-05 21:26:10 -0300

    Atualizando 12 e propondo alternativa

--- Questao13.py
+++ Questao13.py
@@ -1,15 +1,22 @@
+# 13.Escreva um programa que receba a idade de uma pessoa e o preço de um
+# ingresso (inteiros). Aplique descontos: 50% para menores de 12 anos, 30% para
+# maiores de 60 anos, 10% para estudantes (pergunte se é estudante com 'S' ou
+# 'N'). Imprima o preço final.
+
 idade = int(input("Quantos anos você tem? Digite somente o número:"))
-preco_ingresso = float(input("Qual o preço normal do ingresso? Digite os números e use ponto ao invés de vírgula, se precisar:"))
-estudante = input("Uma última coisa: Você é estudante? Digite SOMENTE 'S' ou 'N'.")
+
+preco_ingresso = float(input("Qual o preço normal do ingresso? Digite (use ponto ao invés de vírgula):"))
+preco_final = preco_ingresso
+
+estudante = input("Você é estudante? Digite SOMENTE 'S' para SIM ou 'N' para NÃO.")
 estudante = estudante.upper()
 
 if idade <= 12:
-    preco_final = preco_ingresso / 2
-    print("O preço final é ", preco_final)
+    preco_final = preco_ingresso - (preco_ingresso * 0.5)
 elif idade >= 60:
-    preco_final = preco_ingresso * 0.7
-    print("O preço final é ", preco_final)
+    preco_final = preco_ingresso - (preco_ingresso * 0.3)
 elif estudante == "S":
-    preco_final = preco_ingresso * 0.9
-    print("O preço final é ", preco_final)
+    preco_final = preco_ingresso - (preco_ingresso * 0.1)
+
+print("O preço final é", preco_final)
 
